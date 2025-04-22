22a35a5efa460a75f6bc9ce8b9b23db06409a8a1
Andi <ciliandimarian@gmail.com>
2025-04-22 22:23:28 +0300

    made the setup for testing the backend + tested the connection of the DB

---
+++ backend/__tests__/utils/db-handler.js
@@ -0,0 +1,29 @@
+import mongoose from 'mongoose';
+
+export async function connectTestDB() {
+    const mongoURI = 'mongodb+srv://Ardeleanu:megastudent@cluster0.4blu9nd.mongodb.net/food-del-test';
+
+    if (mongoose.connection.readyState === 0) {
+        await mongoose.connect(mongoURI);
+        console.log('Connected to test DB');
+    }
+}
+
+export async function closeTestDB() {
+    if (mongoose.connection.readyState === 1) {
+        await mongoose.connection.close();
+        console.log('Test DB connection closed');
+    }
+}
+
+export async function clearTestDB() {
+    if (mongoose.connection.readyState!==0) {
+        const collections = mongoose.connection.collections;
+
+        for (const key in collections) {
+            await collections[key].deleteMany({});
+        }
+
+        console.log('Cleared test DB collections');
+    }
+}
