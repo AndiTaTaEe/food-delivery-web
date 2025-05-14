1f1bcf2411eea0b1b7bdd311c88bcb65cc0fb605
Andi <ciliandimarian@gmail.com>
2025-05-15 02:21:51 +0300

    implemented the login/register APIs on frontend

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -5,6 +5,8 @@ export const StoreContext = createContext(null);
 
 const StoreContextProvider = (props) => {
   const [cartItems, setCartItems] = useState({});
+  const url = "http://localhost:4000"
+  const [token, setToken] = useState("");
 
   const addToCart = (itemId) => {
     if (!cartItems[itemId]) {
@@ -39,13 +41,22 @@ const StoreContextProvider = (props) => {
     return totalAmmount;
   }
 
+  useEffect(() => {
+    if(localStorage.getItem("token")) {
+      setToken(localStorage.getItem("token"));
+    }
+  },[]);//folosit pentru a ramane logat in site si dupa refresh
+
   const contextValue = {
     food_list,
     cartItems,
     setCartItems,
     addToCart,
     removeFromCart,
-    getTotalCartAmmount
+    getTotalCartAmmount,
+    url,
+    token,
+    setToken
   };
 
   return (
