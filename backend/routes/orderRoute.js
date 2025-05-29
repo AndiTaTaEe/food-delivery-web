a1a6b71dabbbf7d5a6ccda8e72cb457f35db15bd
Andi <ciliandimarian@gmail.com>
2025-05-30 00:45:45 +0300

    display orders in admin panel

--- backend/routes/orderRoute.js
+++ backend/routes/orderRoute.js
@@ -1,4 +1,4 @@
-import { placeOrder, userOrders, verifyOrder } from "../controllers/orderController.js";
+import { listOrders, placeOrder, userOrders, verifyOrder } from "../controllers/orderController.js";
 import express from "express";
 import authMiddleware from "../middleware/auth.js";
 
@@ -8,5 +8,6 @@ const orderRouter = express.Router();
 orderRouter.post("/place", authMiddleware, placeOrder);
 orderRouter.post("/verify", verifyOrder);
 orderRouter.post("/userorders", authMiddleware, userOrders);
+orderRouter.get("/list", listOrders);
 
 export default orderRouter;
